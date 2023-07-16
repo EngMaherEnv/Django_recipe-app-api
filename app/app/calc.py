2cdd32fbdd3c600bb35c31e2cb3d2ac1890eb7e6
Maher Alrefaai <maher.alrefaai@ajanspress.com.tr>
2023-07-16 15:44:10 +0300

    add wait for db command and a unite test for it

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,10 @@
 calculator function
 """
 
+
 def add(x, y):
-    return x+ y 
+    return x + y
 
 
 def sub(x, y):
-    return x- y
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -10,10 +10,9 @@ class CalcTests(SimpleTestCase):
     """ Test the calc module."""
 
     def test_add_numbers(self):
-        res=calc.add(5, 6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
-
     def test_sub_numbers(self):
-        res=calc.sub(19, 15)
+        res = calc.sub(19, 15)
         self.assertEqual(res, 4)
